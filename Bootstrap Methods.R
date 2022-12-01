1fdc14e4044f6e40aa66208621b895a3a707ed4a
Kalen Mullin <KalenLMullin@gmail.com>
2022-12-01 12:19:12 -0500

    Update Bootstrap Methods.R
    Fixed some functions that were not calling the correct noises and lowered the number of generated points as to not overwhelm my pc, can test higher values after the bootstrapping methods are implemented and tested

--- Bootstrap Methods.R
+++ Bootstrap Methods.R
@@ -2,7 +2,7 @@ library(doParallel)
 
 set.seed(87539319)
 CL = detectCores() - 2
-registerDoParallel(cores = cl)
+registerDoParallel(cores = CL)
 
 n = 5000
 b0 = 1
@@ -19,6 +19,29 @@ esp3 = rgamma(n,shape=4,rate=2)-2
 ySkew = b0 + b1 * x+esp3
 
 esp4 = rnorm(n)*x^2/sqrt(mean(x^4))
-esp = esp4-mean(esp4)
+esp4 = esp4-mean(esp4)
 yWide = b0 + b1 * x+esp4
 
+
+data<-data.frame(x,yNorm)
+start1<-Sys.time()
+boot_b <- foreach(i=1:n, .combine=c) %dopar% {
+  bootstrap_data<-data[sample(nrow(data),nrow(data),replace=T),]
+  unname(lm(yNorm~x,bootstrap_data)$coef[2])
+}
+end1<-Sys.time()
+boot_b<-numeric()
+start2<-Sys.time()
+for(i in 1:n){
+  bootstrap_data<-data[sample(nrow(data),nrow(data),replace=T),]
+  boot_b[i]<-lm(yNorm~x,bootstrap_data)$coef[2]
+}
+end2<-Sys.time()
+start1-end1
+start2-end2
+as.numeric(start1-end1)/as.numeric(start2-end2)
+
+foreach(i=4:1, .combine='c', .inorder=FALSE) %dopar% {
+  Sys.sleep(3 * i)
+  i
+}
